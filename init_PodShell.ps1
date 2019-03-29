96f6812f8631bb49f9373e9c9efc74980b3746ad
Sascha <shiller@gmx.de>
2019-03-29 20:59:32 +0100

    fix PubDate in FyydJson

--- init_PodShell.ps1
+++ init_PodShell.ps1
@@ -2,4 +2,6 @@ Remove-Module Podcast -ErrorAction:SilentlyContinue
 Remove-Module fyyd -ErrorAction:SilentlyContinue
 #Import-Module .\podcast\podcast.psd1
 #Import-Module .\fyyd\fyyd.psd1
-Import-Module .\podcast.psd1
+
+$module = Join-Path $PSScriptRoot "podcast.psd1"
+Import-Module $module
